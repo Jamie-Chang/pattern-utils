874a785dcaf362c5861988a24a3795e28cd526f5
Jamie Chang <jamie.cheng.chang@gmail.com>
2024-12-08 21:31:26 +0000

    Use hatchcing to build package

--- patternlib/__init__.py
+++ patternlib/__init__.py
@@ -2,4 +2,17 @@
 
 That's the intention anyway, the first and only matcher provided here is for 
 generators, allowing peeking into the head elements of a generator.
+
+Example::
+```python
+>>> from patternlib import generator as gen
+
+>>> def example_generator():
+...     yield "some resource"
+...     return "done"
+
+>>> match gen.matcher(example_generator()):
+...     case gen.Node(resource, gen.Empty(end_result)):
+...         print(resource, end_result)
+some resource done
 """
