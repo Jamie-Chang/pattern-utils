d1dadc1a084db795fdbbe8c31b599dbb3e070551
Jamie Chang <jamie.cheng.chang@gmail.com>
2024-12-08 00:08:53 +0000

    Add pattern lib module for matching generators

---
+++ patternlib/__init__.py
@@ -0,0 +1,5 @@
+"""`patternlib` is a toolkit for pattern matching.
+
+That's the intention anyway, the first and only matcher provided here is for 
+generators, allowing peeking into the head elements of a generator.
+"""
